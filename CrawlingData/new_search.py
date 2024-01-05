682de23b7df98d9d764acd923c1fe039482a9551
wenlinwwww <lynnw7839@gmail.com>
2024-01-05 14:44:22 +0800

    update file

---
+++ CrawlingData/new_search.py
@@ -0,0 +1,28 @@
+import tweepy
+import json
+
+consumer_key = "8k7SzAflvtcFmaqJsTiqzylYa"
+consumer_secret = "qPqhLwP77RiuWRgLn8KKPscFLX6PQprPXtrYiaJLxH0Hpe2846"
+access_token = "1517355964617297920-mOWbcyxo7Zr9SXsRN8nXIn9zrm2NpT"
+access_token_secret = "zvJ7znhMV4ZmVG6fKl3t4w7e4McNUU7x8cCTGNympdwqD"
+
+# set API connection
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+# set wait_on_rate_limit =True; as twitter may block you from querying if it finds you exceeding some limits
+api = tweepy.API(auth, wait_on_rate_limit=True)
+
+tweets = tweepy.Cursor(api.search_tweets,
+                       q="covid OR coronavirus geocode:-37.840935,144.946457,40km -filter:retweets AND -filter:quote",
+                       lang="en", tweet_mode='extended').items(50000)
+
+# the geocode is for melbourne; format for geocode="latitude,longitude,radius"
+# radius should be in km/mi
+
+
+for tweet in tweets:
+    with open('recent_tweet_comma.json', 'a', encoding='utf-8') as tf:
+        tf.write('\n')
+        json.dump(tweet._json, tf)
+        tf.write(',')
---
+++ DataAnalysis/Twitter Data/sentiment_analysis.py
@@ -0,0 +1,65 @@
+import re
+import json
+from textblob import TextBlob
+from matplotlib import pyplot as plt
+
+# tweet is the file we get after cleaning (cleanTwitterText.py)
+def tweet_sentiment(tweet):
+
+        # create TextBlob object of passed tweet text
+        twitter_data = TextBlob(tweet)
+
+        # set sentiment
+        if twitter_data.sentiment.polarity > 0:
+            return 'positive'
+        elif twitter_data.sentiment.polarity == 0:
+            return 'neutral'
+        else:
+            return 'negative'
+
+if __name__ == "__main__":
+
+    with open('twitterdata_with_text.json', 'r', encoding="utf-8") as f:
+        Twitter = json.load(f)
+
+    tweets = []
+    for item in Twitter:
+        # create a dictionary to store sentiment regard to the tweet.text (a new field in json)
+        sentiment_tweet = {}
+        sentiment_tweet['text'] = item["text"]
+        sentiment_tweet['sentiment'] = tweet_sentiment(item["text"])
+
+        # print(parsed_tweet)
+        # print("===============================================================")
+
+        # appending sentiment_tweet to tweets list for calculating
+        tweets.append(sentiment_tweet)
+
+
+    # print(len(tweets))
+
+    # positive tweets
+    positive_tweet = []
+    for tweet in tweets:
+        if tweet['sentiment'] == 'positive':
+            positive_tweet.append(tweet['sentiment'])
+    positive_per = round((len(positive_tweet)/len(tweets)) * 100, 4)
+    print("Positive tweets percentage: {} %".format(positive_per))
+    # print(type(positive_per))
+
+    negative_tweet = []
+    for tweet in tweets:
+        if tweet['sentiment'] == 'negative':
+            negative_tweet.append(tweet['sentiment'])
+    negative_per = round((len(negative_tweet)/len(tweets)) * 100, 4)
+    print("Negative tweets percentage: {} %".format(negative_per))
+    
+    neutral_per = round(100 - positive_per - negative_per, 4)
+    print("Neutral tweets percentage: {} %".format(neutral_per))
+
+    labels = ["Positive", "Negative", "Neutral"]
+    data = [positive_per, negative_per, neutral_per]
+    fig = plt.figure(figsize = (10, 7))
+    plt.pie(data, labels = labels, shadow = True, autopct = '%1.1f%%',startangle = 90)
+    plt.title("The Pie Chart for Sentiment Analysis Result")
+    plt.show()
